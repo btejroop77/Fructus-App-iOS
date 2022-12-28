9ffe39d58e29cbb10a1ea11bb59a91d092f31c03
TEJROOP <btejroop77@gmail.com>
2022-12-29 02:22:25 +0530

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by TEJROOP BONAGIRI on 29/12/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
